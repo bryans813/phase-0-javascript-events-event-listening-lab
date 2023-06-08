11f1af355b2b3f8fbb41de1fe947258bde295d7a
Bryan Sosa <bry.sosa@outlook.com>
2023-06-07 21:01:22 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    input.addEventListener('click',clickAlert);
 }
+function clickAlert() {
+    alert("I was clicked");
+}
+const input = document.getElementById('button');
